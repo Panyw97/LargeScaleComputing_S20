7fd38756a00c85e8701cd3918fbbf278367368f9
Yanwei Pan <cleverpan@hanhikos-MacBook-Pro.local>
2020-05-03 01:43:25 -0500

    temp

--- Assignment 1/clock.py
+++ Assignment 1/clock.py
@@ -18,27 +18,27 @@ def sim_life_parallel(n_runs):
     N = int(n_runs/size)
 
 	# Set model parameters
-	rho = 0.5
-	mu = 3.0
-	sigma = 1.0
-	z_0 = mu
+    rho = 0.5
+    mu = 3.0
+    sigma = 1.0
+    z_0 = mu
 	# Set simulation parameters, draw all idiosyncratic random shocks, 
 	# and create empty containers
-	T = int(4160) # Set the number of periods for each simulation np.random.seed(25)
-	eps_mat = sts.norm.rvs(loc=0, scale=sigma, size=(T, N))
-	z_mat = np.zeros((T, N))
-	z_mat[0, :] = z_0
-	for s_ind in range(N): 
-		z_tm1 = z_0
-		for t_ind in range(T):
-			e_t = eps_mat[t_ind, s_ind]
-			z_t = rho * z_tm1 + (1 - rho) * mu + e_t
-			z_mat[t_ind, s_ind] = z_t
-			z_tm1 = z_t
+    T = int(4160) # Set the number of periods for each simulation np.random.seed(25)
+    eps_mat = sts.norm.rvs(loc=0, scale=sigma, size=(T, N))
+    z_mat = np.zeros((T, N))
+    z_mat[0, :] = z_0
+    for s_ind in range(N): 
+        z_tm1 = z_0
+        for t_ind in range(T):
+            e_t = eps_mat[t_ind, s_ind]
+            z_t = rho * z_tm1 + (1 - rho) * mu + e_t
+            z_mat[t_ind, s_ind] = z_t
+            z_tm1 = z_t
 
-	time_elapsed = time.time() - t0
-	print(time_elapsed)
-	return
+    time_elapsed = time.time() - t0
+    print(time_elapsed)
+    return
 
 def main():
     sim_life_parallel(n_runs = 1000)
