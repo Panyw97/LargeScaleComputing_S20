70046d9079a5be50799731c32c92bd56bc6e3d0e
Yanwei Pan <cleverpan@hanhikos-MacBook-Pro.local>
2020-05-03 20:10:57 -0500

    3

--- Assignment 1/grid_search_opencl.py
+++ Assignment 1/grid_search_opencl.py
@@ -14,12 +14,13 @@ def grid_search(n_runs):
 
     T = int(4160)  # Set the number of periods for each simulation
     rand_gen = clrand.PhiloxGenerator(ctx)
-    ran = rand_gen.normal(queue, (n_runs * T), mu=0, sigma=1)
-    rho_l = np.linspace(-0.95, 0.95, 200)
+    ran = rand_gen.normal(queue, (n_runs * T), np.float32, mu=0, sigma=1)
+    rho_l = np.linspace(-0.95, 0.95, 200).astype(np.float32)
 
     opt = []
     scan_sim = cl.Program(ctx, """
-          __kernel void grid_search(__global float *ary_a, __global float *ary_b, float rho, __global float *result)
+          __kernel void grid_search(__global float *ary_a, __global float *ary_b, 
+          float rho, __global float *result)
         {
           int idx = get_global_id(0);
           for (int i=0; i<4160; i++)
@@ -38,10 +39,10 @@ def grid_search(n_runs):
         }
           """).build()
 
-    result = cl_array.to_device(queue, np.empty(n_runs))
-    ary_b = cl_array.to_device(queue, np.empty(n_runs * T))
+    result = cl_array.to_device(queue, np.empty(n_runs).astype(np.float32))
+    ary_b = cl_array.to_device(queue, np.empty(n_runs * T).astype(np.float32))
     for r in rho_l:
-        scan_sim.grid_search(queue, (n_runs,), None, ran.data, ary_b.data, r, result.data)
+        scan_sim.grid_search(queue, (n_runs,), None, ran.data, ary_b.data, np.float32(r), result.data)
         opt.append(result.get().mean())
 
     time_elapsed = time.time() - t0
